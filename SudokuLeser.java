4d1c0d47969bc2288f474c017f68d00305964076
Chinqui <katerinj10@hotmail.com>
2024-02-28 01:24:08 +0100

    datei SudokuLeser hinzufügt

---
+++ SudokuLeser.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/**
+ * SudokuLeser
+ */
+import java.util.ArrayList;
+
+public class SudokuLeser {
+
+    private ArrayList <Sudoku> sudokus;
+
+    private String dateipfad; 
+
+
+    public ArrayList <Sudoku> LeseSudokuAusDatei(){
+        
+    }
+
+    public SudokuLeser(String Pfad){
+        
+    }
+
+
+}
