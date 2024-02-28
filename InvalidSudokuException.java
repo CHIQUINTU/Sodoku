c749c08e25ea59a3a515d93f5fd589bb7136e286
Chinqui <katerinj10@hotmail.com>
2024-02-28 01:55:13 +0100

    datei SudokuException hinzufügt

---
+++ InvalidSudokuException.java
@@ -0,0 +1,16 @@
+/**
+ * InvalidSudokuException
+ * 
+ *
+ */
+public class InvalidSudokuException extends Exception {
+
+    public InvalidSudokuException (){
+        super();
+    }
+
+    public InvalidSudokuException (String messages){
+        super(message);
+    }
+     
+}
